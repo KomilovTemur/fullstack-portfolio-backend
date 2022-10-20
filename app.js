fd4be7d849cf1a3e31885c825c848a218e040b09
Temur Komilov <itteamdev74@gmail.com>
2022-10-20 21:12:43 +0500

    main js file

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require("cors")
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const adminPanelRouter = require("./routes/admin/index")
 
 const app = express();
 
@@ -21,6 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/admin/', adminPanelRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
