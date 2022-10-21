20a91b0633833e1617dc0a153b29b41c68dde533
Temur Komilov <itteamdev74@gmail.com>
2022-10-21 07:07:41 +0500

    main js file

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const adminPanelRouter = require("./routes/admin/index")
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, './views/'));
 app.set('view engine', 'ejs');
 
 app.use(cors())
